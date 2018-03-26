97539a094d9feb15c9eae7bad3f82bd7f9a58118
e-suryasamad <34471570+e-suryasamad@users.noreply.github.com>
2018-03-26 23:46:04 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import AppWeather from './src/appWeather2';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <AppWeather />
+    );
+  }
+}
